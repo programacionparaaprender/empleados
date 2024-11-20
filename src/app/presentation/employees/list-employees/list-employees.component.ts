7cd70600b651b661d8c16a38f803981baa542496
programacionparaaprender <yancel209@gmail.com>
2024-11-20 17:59:42 -0500

    agregando cambios

--- src/app/presentation/employees/list-employees/list-employees.component.ts
+++ src/app/presentation/employees/list-employees/list-employees.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-list-employees',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './list-employees.component.html',
   styleUrl: './list-employees.component.css'
 })
